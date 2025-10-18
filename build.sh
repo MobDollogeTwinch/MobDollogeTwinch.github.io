b6ea815dce3780ddfe0e411a2823353185424497
MobDollogeTwinch <1018312977@qq.com>
2025-10-18 20:18:29 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ git checkout draft -- "$1.md"
 pandoc "$1.md" -o "$1.html" --template "./build/$2.html" --toc --standalone
 
 pandoc "$1.md" --template=metadata.pandoc-tpl --metadata link="$1.html" \
+--highlight-style=kate \
 | sed 's/,$//' \
 | jq --argjson new "$(cat)" '[. + [$new] | sort_by(.link) | group_by(.link)[] | max_by(.date)]' index.json \
 > index.json.tmp && mv index.json.tmp index.json
